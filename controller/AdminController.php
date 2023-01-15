a9c9f1dd6ddcb2ee257d0ff80179f058c587eadd
Florian Burdairon <florian.burdairon@gmail.com>
2023-01-15 16:42:11 +0100

    add AdminController + view

---
+++ controller/AdminController.php
@@ -0,0 +1,30 @@
+<?php
+require_once 'model/customer.php';
+require_once 'model/login.php';
+require_once 'model/order.php';
+require_once 'model/orderItem.php';
+require_once 'view/view.php';
+
+class AdminController
+{
+    public function logout()
+    {
+        unset($_SESSION["admin"]);
+        unset($_SESSION["shoppingcart"]);
+        $_SESSION["backToPage"] = "";
+        header("Location: ".ROOT."home");
+    }
+    
+    public function select_orders(){
+        $orders = Order::select_orders();
+        $view = new View("OrderList", "Mes commandes");
+        $view->generate(array("orders" => $orders));
+    }
+
+    public function select_order_by_id($id){
+        $order = Order::select_order_by_id($id);
+        $orderitems = OrderItem::select_items_by_order($order->get_id());
+        $view = new View("Order", "Détails de la commande");
+        $view->generate(array("order" => $order, "orderitems" => $orderitems));
+    }
+}
